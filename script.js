1e0066859b7006c56893f54752a3bf3faf1cb76b
Your Name <you@example.com>
2018-11-15 17:17:20 +0400

    եղանակ

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-var amenArr=[];
-var satanaArr=[];
-var gishatichArr=[];
+var amenArr = [];
+var satanaArr = [];
+var gishatichArr = [];
 var grassEatArr = [];
 var grassArr = [];
 var side = 25;
@@ -9,8 +9,12 @@ var matrix = [];
 var n = 25;
 
 
-
-     
+var exanak = [1, 2, 3, 4];
+var exanak2 = ["Garun", "Amar", "Ashun", "Dzmer"];
+var ex = prompt("Grel exanak  1=Garun  ,2=Amar, 3=Ashun,  4=Dzmer  ");
+document.write(exanak2[parseInt(ex) - 1]);
+var currentEgh = exanak2[parseInt(ex) - 1];
+var day = 0
 // var matrix = [
 //     [0, 0, 0, 0, 0],
 //     [1, 0, 3, 0, 0],
@@ -27,7 +31,7 @@ var n = 25;
 //     [0, 0, 0, 0, 0],
 //     [0, 0, 0, 0, 0]
 //  ];
- 
+
 
 
 function setup() {
@@ -36,14 +40,14 @@ function setup() {
     background('#acacac');
 
     for (var y = 0; y < n; y++) {
-    matrix[y] = [];
+        matrix[y] = [];
         for (var x = 0; x < n; x++) {
-    
-            var k = random([0,1,1,1,1,1,1,5,5,5,1,1,1,1,1,0,0,0,0,0,2,2,2,0,0,0,4,5,0,0,0,0,0,1,1,5,3,3,2,4,0,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2]);
+
+            var k = random([0, 1, 1, 1, 1, 1, 1, 5, 5, 5, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 4, 5, 0, 0, 0, 0, 0, 1, 1, 5, 3, 3, 2, 4, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2]);
             matrix[y][x] = k;
-    
+
         }
-    }  
+    }
 
     for (var y = 0; y < matrix.length; ++y) {
         for (var x = 0; x < matrix[y].length; ++x) {
@@ -74,12 +78,45 @@ function setup() {
 }
 
 
+
+
+
 function draw() {
+    day++
+    if (day < 100) {
+        currentEgh = exanak2[parseInt(ex) - 1];
+
+    }
+    else if(day > 100 && day < 200) {
+        currentEgh = exanak2[parseInt(ex)];
+
+    }
+    else if (day > 200 && day <300) {
+        currentEgh = exanak2[parseInt(ex) +1];
+
+    }
+    else if (day >300 && day< 400) {
+        currentEgh = exanak2[parseInt(ex) + 2];
+
+    }
+    else if (day > 400) {
+        day=0;
+
+    }
+
+
+
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
 
             if (matrix[y][x] == 1) {
-                fill("green");
+                if (currentEgh == "Dzmer") {
+                    fill("white");
+                }
+                else {
+                    fill("green");
+                }
+
                 rect(x * side, y * side, side, side);
             }
             else if (matrix[y][x] == 2) {
@@ -115,34 +152,34 @@ function draw() {
         grassArr[i].mul();
     }
 
-    
-   
+
+
     for (var i in grassEatArr) {
-         grassEatArr[i].eat();
-        
-        
+        grassEatArr[i].eat();
+
+
     }
 
     for (var i in gishatichArr) {
         gishatichArr[i].eat();
-       
-       
-   }
-   for (var i in satanaArr) {
-    satanaArr[i].eat();
-   
-   
-}
-for (var i in amenArr) {
-    amenArr[i].eat();
-   
-   
-}
 
-   
-    
-    
-    
+
+    }
+    for (var i in satanaArr) {
+        satanaArr[i].eat();
+
+
+    }
+    for (var i in amenArr) {
+        amenArr[i].eat();
+
+
+    }
+
+
+
+
+
 
 
 
